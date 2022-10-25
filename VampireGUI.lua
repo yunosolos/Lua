3137e3e904fb280de6e5a936905c092894919df8
yunosolos <109194950+yunosolos@users.noreply.github.com>
2022-10-25 17:32:22 -0400

    Update VampireGUI.lua

--- VampireGUI.lua
+++ VampireGUI.lua
@@ -1,48 +1,45 @@
-function EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
-
-
 if Game.PlaceId == 2788229376 then
-    local OrionLib = loadstring(game:HttpGet((EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,12075,10920,11340,10605,12600,12495,10185,11970,10605,4935,8295,11970,11025,11655,11550,4935,11445,10185,11025,11550,4935,12075,11655,12285,11970,10395,10605}))))()
-    local Window = OrionLib:MakeWindow({Name = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({9030,10185,11445,11760,11025,11970,10605,3360,7455,8925,7665}), HidePremium = true, SaveConfig = true, ConfigFolder = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({9030,10185,11445,11760,7035,11655,11550,10710,11025,10815})})
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Vampire GUI", HidePremium = true, SaveConfig = true, ConfigFolder = "VampConfig"})
     local Tab = Window:MakeTab({
-        Name = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8085,10185,11025,11550}),
-        Icon = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5775,5775,5145,5985,5250,5565,5040,5565,5565,5355}),
+        Name = "Main",
+        Icon = "rbxassetid://7719250553",
         PremiumOnly = false
     })
     Tab:AddButton({
-        Name = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7560,11025,10500,10605,3360,12285,12075,10605,11970}),
+        Name = "Hide user",
         Callback = function()
-                  if game.Players.LocalPlayer.Character:FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7665,11550,4725,10815,10185,11445,10605,8085,10185,12075,11235})) then
-            game.Players.LocalPlayer.Character:FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7665,11550,4725,10815,10185,11445,10605,8085,10185,12075,11235})):FindFirstChildWhichIsA(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8085,11655,10500,10605,11340})):Destroy()
-            game.Players.LocalPlayer.Character:FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7665,11550,4725,10815,10185,11445,10605,8085,10185,12075,11235})):FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7560,10185,11550,10500,11340,10605})):Destroy()
+                  if game.Players.LocalPlayer.Character:FindFirstChild("In-gameMask") then
+            game.Players.LocalPlayer.Character:FindFirstChild("In-gameMask"):FindFirstChildWhichIsA("Model"):Destroy()
+            game.Players.LocalPlayer.Character:FindFirstChild("In-gameMask"):FindFirstChild('Handle'):Destroy()
                   end
         end
     })
     local Tab = Window:MakeTab({
-    Name = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7035,11655,11445,10290,10185,12180}),
-    Icon = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5775,5775,5145,5985,5250,5565,5040,5565,5565,5355}),
+    Name = "Combat",
+    Icon = "rbxassetid://7719250553",
     PremiumOnly = false
     })
     Tab:AddToggle({
-        Name = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7350,11025,12075,12180,3360,11970,10605,10185,10395,10920,3465}),
+        Name = "Fist reach!",
         Default = false,
         Callback = function(Value)
             if e == true then
-                game:GetService(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8610,12285,11550,8715,10605,11970,12390,11025,10395,10605})):BindToRenderStep(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8610,10605,10185,10395,10920}), 0 , function()
+                game:GetService('RunService'):BindToRenderStep("Reach", 0 , function()
                     local success, err = pcall(function()
                         if game.Players.LocalPlayer.Character.BodyEffects.Attacking.Value == true then
-                            for i,v in pairs(game:GetService(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8400,11340,10185,12705,10605,11970,12075})):GetChildren()) do
+                            for i,v in pairs(game:GetService('Players'):GetChildren()) do
                                 if (v.Character.HumanoidRootPart.Position - game.Players.LocalPlayer.Character.LeftHand.Position).Magnitude <= 50 then
-                                    if game.Players.LocalPlayer.Character:FindFirstChildOfClass(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8820,11655,11655,11340})) then
-                                        if game.Players.LocalPlayer.Character:FindFirstChildOfClass(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8820,11655,11655,11340})):FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7560,10185,11550,10500,11340,10605})) then
-                                            firetouchinterest(game.Players.LocalPlayer.Character:FindFirstChildOfClass(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8820,11655,11655,11340})).Handle, v.Character.UpperTorso, 0)
+                                    if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") then
+                                        if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"):FindFirstChild('Handle') then
+                                            firetouchinterest(game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool").Handle, v.Character.UpperTorso, 0)
                                         else
-                                            firetouchinterest(game.Players.LocalPlayer.Character[EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8610,11025,10815,10920,12180,7560,10185,11550,10500})], v.Character.UpperTorso, 0)
-                                            firetouchinterest(game.Players.LocalPlayer.Character[EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7980,10605,10710,12180,7560,10185,11550,10500})], v.Character.UpperTorso, 0)
-                                            firetouchinterest(game.Players.LocalPlayer.Character[EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8610,11025,10815,10920,12180,7350,11655,11655,12180})], v.Character.UpperTorso, 0)
-                                            firetouchinterest(game.Players.LocalPlayer.Character[EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7980,10605,10710,12180,7350,11655,11655,12180})], v.Character.UpperTorso, 0)
-                                            firetouchinterest(game.Players.LocalPlayer.Character[EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8610,11025,10815,10920,12180,7980,11655,12495,10605,11970,7980,10605,10815})], v.Character.UpperTorso, 0)
-                                            firetouchinterest(game.Players.LocalPlayer.Character[EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7980,10605,10710,12180,7980,11655,12495,10605,11970,7980,10605,10815})], v.Character.UpperTorso, 0)
+                                            firetouchinterest(game.Players.LocalPlayer.Character['RightHand'], v.Character.UpperTorso, 0)
+                                            firetouchinterest(game.Players.LocalPlayer.Character['LeftHand'], v.Character.UpperTorso, 0)
+                                            firetouchinterest(game.Players.LocalPlayer.Character['RightFoot'], v.Character.UpperTorso, 0)
+                                            firetouchinterest(game.Players.LocalPlayer.Character['LeftFoot'], v.Character.UpperTorso, 0)
+                                            firetouchinterest(game.Players.LocalPlayer.Character['RightLowerLeg'], v.Character.UpperTorso, 0)
+                                            firetouchinterest(game.Players.LocalPlayer.Character['LeftLowerLeg'], v.Character.UpperTorso, 0)
                                         end
                                     end
                                 end
@@ -51,65 +48,65 @@ if Game.PlaceId == 2788229376 then
                     end)
                 end)
             elseif e == false then
-                game:GetService(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8610,12285,11550,8715,10605,11970,12390,11025,10395,10605})):UnbindFromRenderStep(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8610,10605,10185,10395,10920}))
+                game:GetService('RunService'):UnbindFromRenderStep("Reach")
             end
         end    
     })
     Tab:AddToggle({
-        Name = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({6825,11550,12180,11025,3360,8715,11340,11655,12495,3465}),
+        Name = "Anti Slow!",
         Default = false,
         Callback = function(Value)
             if gh == true then
-                game:GetService(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8610,12285,11550,8715,10605,11970,12390,11025,10395,10605})):BindToRenderStep(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({6825,11550,12180,11025,4725,8715,11340,11655,12495}), 0 , function()
-                    if game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8190,11655,9135,10185,11340,11235,8715,11760,10605,10605,10500})) then game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8190,11655,9135,10185,11340,11235,8715,11760,10605,10605,10500})):Destroy() end
-                    if game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8610,10605,10500,12285,10395,10605,9135,10185,11340,11235})) then game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8610,10605,10500,12285,10395,10605,9135,10185,11340,11235})):Destroy() end
-                    if game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8190,11655,7770,12285,11445,11760,11025,11550,10815})) then game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8190,11655,7770,12285,11445,11760,11025,11550,10815})):Destroy() end
+                game:GetService('RunService'):BindToRenderStep("Anti-Slow", 0 , function()
+                    if game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild("NoWalkSpeed") then game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild("NoWalkSpeed"):Destroy() end
+                    if game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild("ReduceWalk") then game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild("ReduceWalk"):Destroy() end
+                    if game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild("NoJumping") then game.Players.LocalPlayer.Character.BodyEffects.Movement:FindFirstChild("NoJumping"):Destroy() end
                     if game.Players.LocalPlayer.Character.BodyEffects.Reload.Value == true then game.Players.LocalPlayer.Character.BodyEffects.Reload.Value = false end
                 end)
             elseif gh == false then
-                game:GetService(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8610,12285,11550,8715,10605,11970,12390,11025,10395,10605})):UnbindFromRenderStep(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({6825,11550,12180,11025,4725,8715,11340,11655,12495}))
+                game:GetService('RunService'):UnbindFromRenderStep("Anti-Slow")
             end
         end 
     })
     local Tab = Window:MakeTab({
-    Name = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7350,12285,11550}),
-    Icon = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5775,5775,5145,5985,5250,5565,5040,5565,5565,5355}),
+    Name = "Fun",
+    Icon = "rbxassetid://7719250553",
     PremiumOnly = false
     })
     Tab:AddButton({
-        Name = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7665,11550,12390,11025,12075,11025,10290,11340,10605,3360,11445,10185,12075,11235}),
+        Name = "Invisible mask",
         Callback = function()
             local d = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
-            local k = game.Workspace.Ignored.Shop[EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({9555,8715,12285,11970,10815,10605,11655,11550,3360,8085,10185,12075,11235,9765,3360,4725,3360,3780,5250,5565})]
+            local k = game.Workspace.Ignored.Shop["[Surgeon Mask] - $25"]
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = k.Head.CFrame + Vector3.new(0, 3, 0)
             if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - k.Head.Position).Magnitude <= 50 then
                 wait(.2)
-                fireclickdetector(k:FindFirstChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7035,11340,11025,10395,11235,7140,10605,12180,10605,10395,12180,11655,11970})), 4)
-                toolf = game.Players.LocalPlayer.Backpack:WaitForChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8085,10185,12075,11235}))
+                fireclickdetector(k:FindFirstChild("ClickDetector"), 4)
+                toolf = game.Players.LocalPlayer.Backpack:WaitForChild("Mask")
                 toolf.Parent = game.Players.LocalPlayer.Character
                 wait()
-                game.Players.LocalPlayer.Character:WaitForChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8085,10185,12075,11235}))
-                game:GetService(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({9030,11025,11970,12180,12285,10185,11340,8925,12075,10605,11970})):ClickButton1(Vector2.new())
-                game.Players.LocalPlayer.Character:WaitForChild(EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7665,11550,4725,10815,10185,11445,10605,8085,10185,12075,11235}))
-                game.Players.LocalPlayer.Character[EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7665,11550,4725,10815,10185,11445,10605,8085,10185,12075,11235})].Handle:Destroy()
+                game.Players.LocalPlayer.Character:WaitForChild("Mask")
+                game:GetService("VirtualUser"):ClickButton1(Vector2.new())
+                game.Players.LocalPlayer.Character:WaitForChild("In-gameMask")
+                game.Players.LocalPlayer.Character["In-gameMask"].Handle:Destroy()
                 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(d)
             end
           end
     })
     local Tab = Window:MakeTab({
-    Name = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8820,10605,11340,10605,11760,11655,11970,12180,12075}),
-    Icon = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5775,5775,5145,5985,5250,5565,5040,5565,5565,5355}),
+    Name = "Teleports",
+    Icon = "rbxassetid://7719250553",
     PremiumOnly = false
     })
     local Tab = Window:MakeTab({
-    Name = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7035,11970,10605,10500,11025,12180,12075}),
-    Icon = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5775,5775,5145,5985,5250,5565,5040,5565,5565,5355}),
+    Name = "Credits",
+    Icon = "rbxassetid://7719250553",
     PremiumOnly = false
     })
     OrionLib:MakeNotification({
-        Name = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({7035,11970,10605,10185,12180,11655,11970,12075}),
-        Content = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({8820,10920,11025,12075,3360,12495,10185,12075,3360,12075,10395,11970,11025,11760,12180,10605,10500,3360,10185,11550,10500,3360,11445,10185,10500,10605,3360,10290,12705,3360,11760,12285,11970,10605,12075,12180,3675,5040,5040,5040,5145,3360,10185,11550,10500,3360,11025,12285,12600,3675,5040,5040,5040,5145,3360,7245,11550,11130,11655,12705,3465}),
-        Image = EZidDBwNoNaxZovwalSZjyuwdmgBmPJaVBWPPgUWkDIa({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5775,5775,5145,5985,5250,5565,5040,5565,5565,5355}),
+        Name = "Creators",
+        Content = "This was scripted and made by purest#0001 and iux#0001 Enjoy!",
+        Image = "rbxassetid://7719250553",
         Time = Inf
     })
-    end    
+    end
